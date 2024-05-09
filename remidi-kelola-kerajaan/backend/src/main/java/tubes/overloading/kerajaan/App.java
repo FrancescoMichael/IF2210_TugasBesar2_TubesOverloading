e28d5d5c0be8301d1a1210c33aaf8cb02e5031ad
Francesco Michael Kusuma <francescokusuma15@gmail.com>
2024-05-09 17:55:00 +0700

    modularity

---
+++ remidi-kelola-kerajaan/backend/src/main/java/tubes/overloading/kerajaan/App.java
@@ -0,0 +1,17 @@
+package tubes.overloading.kerajaan;
+
+import person.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Person person = new Person("Marvel");
+
+        System.out.println(person.getName());
+    }
+}
