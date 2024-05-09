036215a371ab9a1edba4fa157642e4de647f637b
b33rk <brkmanics@gmail.com>
2024-05-09 15:46:30 +0700

    kerangka grid

---
+++ src/main/java/oop/Grid/Grid.java
@@ -0,0 +1,34 @@
+package oop.Grid;
+
+public class Grid {
+    private int row;
+    private int col;
+    public void set() {
+        
+    }
+    public void remove() {
+        
+    }
+    public void getElement() {
+        
+    }
+}
+
+class Field extends Grid {
+    //private list kartu
+    public void harvest() {
+
+    }
+    public void growPlant() {
+
+    }
+    
+}
+
+class ActiveDeck extends Grid {
+    //private list kartu
+    public void useCard() {
+
+    }
+
+}
