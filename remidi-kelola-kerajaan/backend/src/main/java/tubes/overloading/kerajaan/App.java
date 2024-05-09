a7f8ae848a9080cacccb15899a77227038ab4de6
Francesco Michael Kusuma <francescokusuma15@gmail.com>
2024-05-09 18:15:02 +0700

    temp

--- remidi-kelola-kerajaan/backend/src/main/java/tubes/overloading/kerajaan/App.java
+++ remidi-kelola-kerajaan/backend/src/main/java/tubes/overloading/kerajaan/App.java
@@ -10,7 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        Person person = new Person("Marvel");
+        Person person = new Person("Ini tubes OOP");
 
         System.out.println(person.getName());
     }
