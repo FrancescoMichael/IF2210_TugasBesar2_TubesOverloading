99d719ad1949018b7a5fe0241668806890e7ea27
denoseu <denisefeliciatiowanni@gmail.com>
2024-05-13 21:48:50 +0700

    init class

--- src/main/java/oop/Card/Item.java
+++ src/main/java/oop/Card/Item.java
@@ -5,5 +5,9 @@ public class Item extends Card {
     public Item(String pathToImg, String name) {
         super(pathToImg, name);
     }
+
+    public void use() {
+        System.out.println("Item used");
+    }
     
 }
