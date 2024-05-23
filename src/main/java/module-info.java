a8faf7ce22f25aa4b070bae38925d082b66752a9
Francesco Michael Kusuma <francescokusuma15@gmail.com>
2024-05-24 04:51:53 +0700

    plugin

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module oop {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+//    requires org.json;
+    // requires org.yaml.snakeyaml;
 
     opens oop to javafx.fxml;
     exports oop;
